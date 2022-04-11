b6cdf7ea6e3a866a2bad1f7705b3fdc27a0626b1
Abhirit Das <72955707+abhiritdas@users.noreply.github.com>
2022-04-11 10:57:17 -0500

    Add files via upload

--- MusicApp/main.py
+++ MusicApp/main.py
@@ -1,6 +1,22 @@
-import music21
-sBach = music21.corpus.parse('bach/bwv7.7')
-mf = music21.midi.translate.streamToMidiFile(sBach)
-mf.open("bach.mid", "wb")
-mf.write()
-mf.close()
+import tkinter as tk
+import fnmatch
+import os
+from pygame import mixer
+
+canvas = tk.Tk()
+canvas.title("Staff Notes")
+canvas.geometry("400x600")
+canvas.config(bg = "white")
+
+listBox = tk.Listbox(canvas, fg= "white" , bg = "black", width = 100, font = ('poppins', 20))
+listBox.pack(padx=0, pady=15)
+
+listBox.insert(0, 'C')
+listBox.insert(1, 'D')
+listBox.insert(2, 'E')
+listBox.insert(3, 'F')
+listBox.insert(4, 'G')
+listBox.insert(5, 'A')
+listBox.insert(6, 'B')
+
+canvas.mainloop()
