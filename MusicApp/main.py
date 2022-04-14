6b7c17ab5d32e8f160d54ffb297da057f0debe6d
Abhirit Das <72955707+abhiritdas@users.noreply.github.com>
2022-04-14 11:04:31 -0500

    Add files via upload

--- MusicApp/main.py
+++ MusicApp/main.py
@@ -6,17 +6,36 @@ from pygame import mixer
 canvas = tk.Tk()
 canvas.title("Staff Notes")
 canvas.geometry("400x600")
-canvas.config(bg = "white")
+canvas.config(bg = "black")
 
-listBox = tk.Listbox(canvas, fg= "white" , bg = "black", width = 100, font = ('poppins', 20))
-listBox.pack(padx=0, pady=15)
+#rootpath = "address"
+#pattern = "*.mp3"
 
-listBox.insert(0, 'C')
-listBox.insert(1, 'D')
-listBox.insert(2, 'E')
-listBox.insert(3, 'F')
-listBox.insert(4, 'G')
-listBox.insert(5, 'A')
-listBox.insert(6, 'B')
+def which_button(button_press):
+    print(button_press)
 
+def play_audio():
+    #mixer.music.load(rootpath)
+    print("playing")
+
+def stop():
+    mixer.music.stop()    
+
+Play = tk.Button(canvas, text="PLAY", command=play_audio)
+A = tk.Button(canvas, text ="A", command=lambda m="A": which_button(m))
+B = tk.Button(canvas, text ="B", command=lambda m="B": which_button(m))
+C = tk.Button(canvas, text ="C", command=lambda m="C": which_button(m))
+D = tk.Button(canvas, text ="D", command=lambda m="D": which_button(m))
+E = tk.Button(canvas, text ="E", command=lambda m="E": which_button(m))
+F = tk.Button(canvas, text ="F", command=lambda m="F": which_button(m))
+G = tk.Button(canvas, text ="G", command=lambda m="G": which_button(m))
+
+Play.pack()
+A.pack()
+B.pack()
+C.pack()
+D.pack()
+E.pack()
+F.pack()
+G.pack()
 canvas.mainloop()
