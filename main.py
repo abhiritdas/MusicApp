1118dc222c337f328a882835ea21d2da14617bf6
abhiritdas <abhiritdas@gmail.com>
2022-05-17 01:00:37 -0500

    auto commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 import os
 import random
 from pygame import mixer
@@ -7,24 +8,25 @@ canvas = tk.Tk()
 canvas.title("Staff Notes")
 canvas.geometry("400x400")
 canvas.config(bg = "black")
-rootpath = "C:\\Users\\abhir\\Documents_Local\\MusicApp\\Audio"
-
 mixer.init()
 
 class Audio:
     def __init__(self):
-        self.random_folder = random.choice(os.listdir(rootpath+"\\"))
-        self.random_file = random.choice(os.listdir(rootpath+"\\"+self.random_folder+"\\"))
-        self.audio_directory = rootpath+"\\"+self.random_folder+"\\"+self.random_file
+        self.random_folder = random.choice(os.listdir("Audio\\"))
+        self.random_file = random.choice(os.listdir("Audio\\"+self.random_folder+"\\"))
+        self.audio_directory = "Audio\\"+self.random_folder+"\\"+self.random_file
 
 class Main():
     def __init__(self):
         self.audio_ready = True
+        self.score = 0
 
     def which_button(self, button_press):
         if(button_press==folder):
             correct_label.pack()
             canvas.after(1000, self.hide_label, correct_label)
+            self.score += 1
+            score_label.config(text = "Your score: " + str(main.score))
             self.audio_ready = True
         else:
             incorrect_label.pack()
@@ -44,24 +46,30 @@ class Main():
 
     def hide_label(self, label):
         label.forget()
-    
+
 main = Main()
-Play = tk.Button(canvas, text="PLAY", command=main.play_audio)
-A = tk.Button(canvas, text ="A", command=lambda m="A": main.which_button(m))
-Bb = tk.Button(canvas, text ="Bb", command=lambda m="Bb": main.which_button(m))
-B = tk.Button(canvas, text ="B", command=lambda m="B": main.which_button(m))
-C = tk.Button(canvas, text ="C", command=lambda m="C": main.which_button(m))
-C_sharp = tk.Button(canvas, text ="C#", command=lambda m="C#": main.which_button(m))
-D = tk.Button(canvas, text ="D", command=lambda m="D": main.which_button(m))
-Eb = tk.Button(canvas, text ="Eb", command=lambda m="Eb": main.which_button(m))
-E = tk.Button(canvas, text ="E", command=lambda m="E": main.which_button(m))
-F = tk.Button(canvas, text ="F", command=lambda m="F": main.which_button(m))
-F_sharp = tk.Button(canvas, text ="F#", command=lambda m="F#": main.which_button(m))
-G = tk.Button(canvas, text ="G", command=lambda m="G": main.which_button(m))
-G_sharp = tk.Button(canvas, text ="G#", command=lambda m="G#": main.which_button(m))
+
+#####################################################################################
+
+
+Play = tk.Button(canvas, text="PLAY SOUND", command=main.play_audio)
+A = tk.Button(canvas, text = "  A   ", command=lambda m="A": main.which_button(m))
+Bb = tk.Button(canvas, text ="  Bb  ", command=lambda m="Bb": main.which_button(m))
+B = tk.Button(canvas, text ="  B   ", command=lambda m="B": main.which_button(m))
+C = tk.Button(canvas, text ="  C   ", command=lambda m="C": main.which_button(m))
+C_sharp = tk.Button(canvas, text ="  C#  ", command=lambda m="C#": main.which_button(m))
+D = tk.Button(canvas, text ="  D   ", command=lambda m="D": main.which_button(m))
+Eb = tk.Button(canvas, text ="  Eb  ", command=lambda m="Eb": main.which_button(m))
+E = tk.Button(canvas, text ="  E   ", command=lambda m="E": main.which_button(m))
+F = tk.Button(canvas, text ="  F   ", command=lambda m="F": main.which_button(m))
+F_sharp = tk.Button(canvas, text ="  F#  ", command=lambda m="F#": main.which_button(m))
+G = tk.Button(canvas, text ="  G   ", command=lambda m="G": main.which_button(m))
+G_sharp = tk.Button(canvas, text ="  G#  ", command=lambda m="G#": main.which_button(m))
 correct_label = tk.Label(canvas, text ="CORRECT")
 incorrect_label = tk.Label(canvas, text ="INCORRECT, TRY AGAIN")
+score_label = tk.Label(canvas, text = "Your Score: " + str(main.score))
     
+score_label.pack()
 Play.pack()
 A.pack()
 Bb.pack()
